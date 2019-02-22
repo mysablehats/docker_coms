454691826c62b77ba503bc3face57d753ad70478
Fred K <frederico-klein@plymouth.ac.uk>
2019-02-22 17:23:53 +0000

    fixed messages

--- addroute.sh
+++ addroute.sh
@@ -2,7 +2,7 @@
 
 ALLDEFAULTNICS=`route | grep '^default' | grep -o '[^ ]*$'`
 set $ALLDEFAULTNICS
-printf 'default nics found:\n%s\nWill add rule for %s, hope I am not making a mistake!\n' "$ALLDEFAULTNICS" "$1"
+
 DOCKERPCIP=10.0.0.7
 THISPCNIC=$1
 DOCKERNET=172.0.0.0
@@ -13,9 +13,10 @@ MYRULE=`ip route | grep ${DOCKERNET}`
 if [ -z "${MYRULE}" ]
 then
       echo "Routing rule not found. Will add one"
+      printf 'default nics found:\n%s\nWill add rule for %s, hope I am not making a mistake!\n' "$ALLDEFAULTNICS" "$THISPCNIC"
       sudo ip route add ${DOCKERNET}/${DOCKERNETMASK} via ${DOCKERPCIP} dev ${THISPCNIC}
 else
-      echo -e "Routing rule found. Will not update it. Remove it by hand if you want to reset it, before running this script with\nsudo ip route del ${DOCKERNET}/${DOCKERNETMASK}"
+      printf 'Routing rule found. Will not update it. Remove it by hand if you want to reset it, before running this script with\nsudo ip route del %s/%s' "${DOCKERNET}" "${DOCKERNETMASK}"
 fi
 
 #sudo ip route
